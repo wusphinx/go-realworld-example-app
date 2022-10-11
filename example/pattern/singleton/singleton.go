627957ef4268a1cc0d929ec31a08e6c18630b124
wusphinx <wu.sphinx@gmail.com>
2022-10-11 15:26:37 +0800

    chore: debug

--- example/pattern/singleton/singleton.go
+++ example/pattern/singleton/singleton.go
@@ -12,7 +12,7 @@ var instance *singleton
 // 用于测试
 var count int
 
-// GetInstance return singleton
+//GetInstance return singleton
 //revive:disable:unexported-return
 func GetInstance() *singleton {
 	once.Do(func() {
