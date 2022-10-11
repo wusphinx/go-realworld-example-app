1ed9fcf60cf1331bff77ac3f8f578cbf80d8a4c0
wusphinx <wu.sphinx@gmail.com>
2022-10-11 15:13:41 +0800

    fix: linx unexported-return

--- example/pattern/singleton/singleton.go
+++ example/pattern/singleton/singleton.go
@@ -12,6 +12,8 @@ var instance *singleton
 // 用于测试
 var count int
 
+// GetInstance return singleton
+//revive:disable:unexported-return
 func GetInstance() *singleton {
 	once.Do(func() {
 		instance = &singleton{}
